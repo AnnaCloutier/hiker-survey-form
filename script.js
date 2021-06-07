e022bacc7b8a27390a786d668069c88510aef5f2
Anna Cloutier <anna.cloutier.nh@gmail.com>
2021-06-07 18:38:08 -0400

    Now mobile and desktop responsive and added JS script to change button text after submit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//add button change to "Thank you!"
+//change button text after submission
+let submitButton = document.querySelector(".button-div");
+
+submitButton.addEventListener("click", buttonText);
+
+function buttonText() {
+    event.preventDefault();
+    document.getElementById("submit").innerHTML = "Thanks!";
+};
